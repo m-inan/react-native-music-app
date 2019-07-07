9e0e7947e06ee95f396144d8b6f23d2521560496
minan <muhammet@tilpark.com>
2019-07-07 20:59:35 +0300

    update gitignore

---
+++ android/app/src/main/java/com/musicapp/MainApplication.java
@@ -0,0 +1,57 @@
+package com.musicapp;
+
+import android.app.Application;
+
+import com.facebook.react.ReactApplication;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+import com.guichaguri.trackplayer.TrackPlayer;
+import com.horcrux.svg.SvgPackage;
+import com.rnfs.RNFSPackage;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+            new RNGestureHandlerPackage(),
+            new TrackPlayer(),
+            new SvgPackage(),
+            new RNFSPackage(),
+            new RNDeviceInfo(),
+            new AsyncStoragePackage()
+      );
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
+}
