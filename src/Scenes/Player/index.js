73cd1477a8db40f4a46e8167c9d6f69181a61d3b
minan <muhammet@tilpark.com>
2019-07-28 01:03:21 +0300

    player zIndex

--- src/Scenes/Player/index.js
+++ src/Scenes/Player/index.js
@@ -37,7 +37,7 @@ const styles = {
 		top: 0,
 		left: 0,
 		position: 'absolute',
-		zIndex: 1000,
+		zIndex: 9998,
 		backgroundColor: 'rgb(35, 40, 44)',
 		transform: [{ translateY: positionY }]
 	}
