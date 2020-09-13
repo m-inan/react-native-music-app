34267c1d1e57f3a36ec40de55bfc4f137c6e3d20
minan <muhammet@tilpark.com>
2020-09-13 13:42:27 +0300

    remove youtube playlist donwload button

--- src/scenes/Playlist/Title.js
+++ src/scenes/Playlist/Title.js
@@ -1,41 +1,12 @@
-import React, { useState } from 'react'
-import { View, Text, TouchableOpacity, ActivityIndicator } from 'react-native'
-import { useDispatch } from 'react-redux'
-
-import { ListDownload } from 'components/Icons'
-
-import { getPlaylistsData, setPlaylist } from 'utils'
-import { setList } from 'reducers/Playlist/actions'
+import React from 'react'
+import { View, Text } from 'react-native'
 
 export default function Title({ items, index }) {
-	const dispatch = useDispatch()
-	const [isProgress, setProgress] = useState(false)
-
-	const setData = async () => {
-		setProgress(true)
-
-		const playlist = await getPlaylistsData()
-
-		dispatch(setList(playlist))
-
-		setProgress(false)
-		setPlaylist(playlist)
-	}
-
 	return (
 		<View style={styles.container}>
 			<Text numberOfLines={1} style={styles.title}>
 				{items.length && items[index].title}
 			</Text>
-			<TouchableOpacity onPress={setData}>
-				<View style={{ paddingHorizontal: 20 }}>
-					{isProgress ? (
-						<ActivityIndicator />
-					) : (
-						<ListDownload style={styles.icon} />
-					)}
-				</View>
-			</TouchableOpacity>
 		</View>
 	)
 }
@@ -50,9 +21,5 @@ const styles = {
 		fontSize: 30,
 		padding: 20,
 		flex: 1
-	},
-	icon: {
-		width: 25,
-		height: 25
 	}
 }
