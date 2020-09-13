43099cc113c0325d3a2f0c72ecc5c25e4c258f61
minan <muhammet@tilpark.com>
2020-09-13 13:44:56 +0300

    remove download button on list item

--- src/scenes/Playlist/Item.js
+++ src/scenes/Playlist/Item.js
@@ -1,31 +1,14 @@
 import React from 'react'
-import {
-	View,
-	Text,
-	Image,
-	ActivityIndicator,
-	TouchableOpacity
-} from 'react-native'
+import { View, Text, Image, TouchableOpacity } from 'react-native'
 import { useDispatch } from 'react-redux'
 
-import { downloadAudio, itemPlay } from 'reducers/Player/actions'
+import { itemPlay } from 'reducers/Player/actions'
 import { Colors } from 'constants'
 
-import { Download, Play } from 'components/Icons'
+import { Play } from 'components/Icons'
 
-export default function Item({
-	artwork,
-	title,
-	videoId,
-	exists,
-	playlistId,
-	loading
-}) {
+export default function Item({ artwork, title, videoId, playlistId }) {
 	const dispatch = useDispatch()
-
-	const _downloadAudio = () => {
-		dispatch(downloadAudio(videoId, playlistId))
-	}
 
 	const _play = async () => {
 		dispatch(itemPlay(videoId, playlistId))
@@ -35,23 +18,11 @@ export default function Item({
 		<View style={styles.item}>
 			<Image source={{ uri: artwork }} style={styles.artwork} />
 			<Text style={{ color: Colors.gray, flex: 1 }}>{title}</Text>
-			{exists ? (
-				<TouchableOpacity onPress={_play}>
-					<View style={styles.play}>
-						<Play />
-					</View>
-				</TouchableOpacity>
-			) : loading ? (
-				<View style={styles.spinner}>
-					<ActivityIndicator />
+			<TouchableOpacity onPress={_play}>
+				<View style={styles.play}>
+					<Play />
 				</View>
-			) : (
-				<TouchableOpacity onPress={_downloadAudio}>
-					<View style={styles.download}>
-						<Download />
-					</View>
-				</TouchableOpacity>
-			)}
+			</TouchableOpacity>
 		</View>
 	)
 }
@@ -68,15 +39,6 @@ const styles = {
 		width: 60,
 		height: 50,
 		marginRight: 15
-	},
-	spinner: {
-		paddingVertical: 20
-	},
-	download: {
-		width: 50,
-		height: 50,
-		padding: 5,
-		paddingLeft: 20
 	},
 	play: {
 		width: 50,
