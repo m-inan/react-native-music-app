b96dcd02c1a3d9bc028bb56ca8810f744d544bdd
m-inan <muhammet@tilpark.com>
2020-12-09 00:49:18 +0300

    remove unnecessary View component

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StatusBar } from 'react-native';
+import { StatusBar } from 'react-native';
 
 import { Provider } from 'src/provider';
 import { Playlist } from 'src/screens/Playlist';
