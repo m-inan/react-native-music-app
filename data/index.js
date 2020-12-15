07edbea55792b6063a8eedc9020b0e6622e904b5
m-inan <muhammet@tilpark.com>
2020-12-16 00:35:41 +0300

    add data json, artworks and sounds

---
+++ data/index.js
@@ -0,0 +1,121 @@
+export const sounds = [
+	{
+		id: 1,
+		title: 'Get Lucky',
+		artist: 'Daft Punk ft. Pharrell Williams, Nile Rodgers',
+		source: require('./sounds/getlucky.mp3'),
+		artwork: require('./artworks/getlucky.png')
+	},
+	{
+		id: 2,
+		title: 'OneRepublic',
+		artist: 'Counting Stars',
+		source: require('./sounds/countingstars.mp3'),
+		artwork: require('./artworks/countingstars.jpg')
+	},
+	{
+		id: 3,
+		title: 'Lights',
+		artist: 'Ellie Goulding',
+		source: require('./sounds/lights.mp3'),
+		artwork: require('./artworks/lights.jpg')
+	},
+	{
+		id: 4,
+		title: 'Grenade',
+		artist: 'Bruno Mars',
+		source: require('./sounds/grenade.mp3'),
+		artwork: require('./artworks/grenade.jpg')
+	},
+	{
+		id: 5,
+		title: 'Addicted To You',
+		artist: 'Avicii',
+		source: require('./sounds/addictedtoyou.mp3'),
+		artwork: require('./artworks/addictedtoyou.jpg')
+	},
+	{
+		id: 6,
+		title: 'Renegades',
+		artist: 'X Ambassadors',
+		source: require('./sounds/renegades.mp3'),
+		artwork: require('./artworks/renegades.jpg')
+	},
+	{
+		id: 7,
+		title: 'Riptide',
+		artist: 'Vance Joy',
+		source: require('./sounds/riptide.mp3'),
+		artwork: require('./artworks/riptide.jpg')
+	},
+	{
+		id: 8,
+		title: 'Dont Stop Me Now',
+		artist: 'Queen',
+		source: require('./sounds/dontstopmenow.mp3'),
+		artwork: require('./artworks/dontstopmenow.jpg')
+	},
+	{
+		id: 9,
+		title: 'Another Brick In The Wall',
+		artist: 'Pink Floyd',
+		source: require('./sounds/anotherbrickinthewall.mp3'),
+		artwork: require('./artworks/anotherbrickinthewall.jpg')
+	},
+	{
+		id: 10,
+		title: 'Piano Sonata No. 1 in F minor',
+		artist: 'Ludwig van Beethoven',
+		source: require('./sounds/pianosonata.mp3'),
+		artwork: require('./artworks/pianosonata.jpg')
+	},
+	{
+		id: 11,
+		title: 'Symphony No. 40 in G minor K.550',
+		artist: 'Wolfgang Amadeus Mozart',
+		source: require('./sounds/symphony.mp3'),
+		artwork: require('./artworks/symphony.jpg')
+	},
+	{
+		id: 12,
+		title: 'Violin Sonata No. 2 in A minor BWV.1003',
+		artist: 'Johann Sebastian Bach',
+		source: require('./sounds/violinsonata.mp3'),
+		artwork: require('./artworks/violinsonata.jpg')
+	},
+	{
+		id: 13,
+		title: 'Cello Sonata No. 3 in A Minor RV.43',
+		artist: 'Antonio Vivaldi',
+		source: require('./sounds/cellosonata.mp3'),
+		artwork: require('./artworks/cellosonata.jpg')
+	}
+]
+
+export const playlist = [
+	{
+		id: 1,
+		title: 'Pop',
+		items: [1, 2, 3, 4, 5]
+	},
+	{
+		id: 2,
+		title: 'Road',
+		items: [1, 6, 7]
+	},
+	{
+		id: 3,
+		title: 'Rock',
+		items: [8, 9]
+	},
+	{
+		id: 4,
+		title: 'Classic',
+		items: [10, 11]
+	},
+	{
+		id: 5,
+		title: 'Baroque',
+		items: [12, 13]
+	}
+]
