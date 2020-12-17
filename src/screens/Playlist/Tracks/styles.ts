1fc32f0c5741ee07d9d8ef9eb9f8125a858ab3ad
m-inan <muhammet@tilpark.com>
2020-12-17 22:50:23 +0300

    remove unused import

--- src/screens/Playlist/Tracks/styles.ts
+++ src/screens/Playlist/Tracks/styles.ts
@@ -1,4 +1,4 @@
-import { StyleSheet, Dimensions } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { Colors } from 'src/constants';
 
 export const styles = StyleSheet.create({
