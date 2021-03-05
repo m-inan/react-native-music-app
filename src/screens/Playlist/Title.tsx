59ebf1b0aab3d95bf3e59e8e2740d110b5fe79d9
m-inan <muhammet@tilpark.com>
2021-03-05 17:27:43 +0300

    playlist title change with swipeIndex

--- src/screens/Playlist/Title.tsx
+++ src/screens/Playlist/Title.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import { View, StyleSheet } from 'react-native';
 
 import { Text, TextType } from 'src/components';
+import { usePlaylist } from 'src/provider';
 
 export const Title = () => {
+  const { lists, swipeIndex } = usePlaylist();
+
+  const text = lists.find((_, key) => key === swipeIndex)?.title ?? 'Null';
+
   return (
     <View style={styles.container}>
       <Text size={30} type={TextType.SEMIBOLD}>
-        Pop
+        {text}
       </Text>
     </View>
   );
